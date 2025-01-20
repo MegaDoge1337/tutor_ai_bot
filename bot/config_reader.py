59ebe4f1e2708235d0fff736aa2e37c6af67fe88
Aleksandr <gggols32@gmail.com>
2025-01-20 14:13:10 +0300

    Add new environment variables for capturing by pydantic-settings

--- bot/config_reader.py
+++ bot/config_reader.py
@@ -4,6 +4,8 @@ from pydantic import SecretStr
 
 class Settings(BaseSettings):
     bot_token: SecretStr
+    llm_base_url: str
+    llm_api_key: str
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
