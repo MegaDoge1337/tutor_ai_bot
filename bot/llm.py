cff3dff468efa37f8f14669341850dd6c430dd12
Aleksandr <gggols32@gmail.com>
2025-01-24 17:10:18 +0300

    Add completion generation params `max_tokens` and `stream` with default values

--- bot/llm.py
+++ bot/llm.py
@@ -5,14 +5,19 @@ class LLM:
     def __init__(self, base_url: str, api_key: str) -> None:
         self.client = OpenAI(base_url=base_url, api_key=api_key)
 
-    def make_completion(self, user_prompt: str, temperature: float = 0.7) -> str:
+    def make_completion(self, user_prompt: str, 
+                        temperature: float = 0.7, 
+                        max_tokens: int = 1024, 
+                        stream: bool = False) -> str:
         completion = self.client.chat.completions.create(
-            model="aya-23-8b",
+            model="deepseek-chat",
             messages=[
                 {"role": "system", "content": SYSTEM_PROMPT},
                 {"role": "user", "content": USER_PROMPT.format(user_prompt=user_prompt)}
             ],
             temperature=temperature,
+            max_tokens=max_tokens,
+            stream=stream
         )
         return completion.choices[0].message.content
     
